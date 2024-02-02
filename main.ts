eadd517a7fe95d13afbee1dec932e73b89365949
Ahmed Saber <ahmed0saber33@gmail.com>
2024-02-02 21:54:37 +0200

    Another try for cors

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
-import { cors } from "https://deno.land/x/hono@v3.12.10/middleware.ts";
 import data from "./data.json" assert { type: "json" };
 
 const app = new Hono();
@@ -12,12 +11,14 @@ app.get("/api/:dinosaur", (c) => {
   const dinosaur = c.req.param("dinosaur").toLowerCase();
   const found = data.find((item) => item.name.toLowerCase() === dinosaur);
   if (found) {
-    return c.json(found);
+    // return c.json(found);
+    return new Response(JSON.stringify(found), {
+      status: 200,
+      headers: { "access-control-allow-origin": "*" },
+    });
   } else {
     return c.text("No dinosaurs found.");
   }
 });
-
-app.use(cors());
 
 Deno.serve(app.fetch);
