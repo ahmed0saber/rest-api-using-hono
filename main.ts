5ecc62b51df69ebc48a47d31e84673d50e0fa9d3
Ahmed Saber <ahmed0saber33@gmail.com>
2024-02-02 22:00:29 +0200

    Fully fix cors issue

--- main.ts
+++ main.ts
@@ -3,22 +3,35 @@ import data from "./data.json" assert { type: "json" };
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Welcome to dinosaur API!"));
+app.get("/", () => {
+  return new Response("Welcome to dinosaur API!", {
+    status: 200,
+    headers: { "access-control-allow-origin": "*" },
+  });
+});
 
-app.get("/api/", (c) => c.json(data));
+app.get("/api/", () => {
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { "access-control-allow-origin": "*" },
+  });
+});
 
 app.get("/api/:dinosaur", (c) => {
   const dinosaur = c.req.param("dinosaur").toLowerCase();
   const found = data.find((item) => item.name.toLowerCase() === dinosaur);
+
   if (found) {
-    // return c.json(found);
     return new Response(JSON.stringify(found), {
       status: 200,
       headers: { "access-control-allow-origin": "*" },
     });
-  } else {
-    return c.text("No dinosaurs found.");
   }
+
+  return new Response("No dinosaurs found.", {
+    status: 200,
+    headers: { "access-control-allow-origin": "*" },
+  });
 });
 
 Deno.serve(app.fetch);
